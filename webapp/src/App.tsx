7da22c9fbb4a2e3f810a1b5c8d24fbe59df78c25
Mofyart <busygin.artem05mail.ru>
2025-01-21 20:02:42 +0300

    First step Ract, JS and set up Prettier

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const users = [
+    {
+      nickName: 'MOFY',
+      descryptionText: 'Текст 1......',
+      data: '21.01.2025',
+      image: '.PDF',
+    },
+    {
+      nickName: 'ALESIO',
+      descryptionText: 'Текст 2......',
+      data: '17.04.2024',
+      image: '.PDF',
+    },
+    {
+      nickName: 'ANDREY',
+      descryptionText: 'Текст 3......',
+      data: '16.02.2025',
+      image: '.PDF',
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <h1>AAATech & Co.</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.nickName}>
+            <h2>{user.nickName}</h2>
+            <p>{user.descryptionText}</p>
+            <p>{user.data}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
