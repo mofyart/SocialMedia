0b4591666d24cca76de3e0ef861f6dea0ce27ad2
Mofyart <busygin.artem05mail.ru>
2025-01-26 20:51:44 +0300

    End forms

--- webapp/src/components/Input/index.tsx
+++ webapp/src/components/Input/index.tsx
@@ -2,22 +2,26 @@ import { type FormikProps } from 'formik'
 
 export const Input = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
   const value = formik.values[name]
+  const error = formik.errors[name] as string | undefined
+  const touch = formik.touched[name]
+
   return (
     <div style={{ marginBottom: 10 }}>
-      <label htmlFor={name}>
-        {label}
-        {value}
-      </label>
+      <label htmlFor={name}>{label}</label>
       <br />
       <input
         type="text"
         onChange={(e) => {
           void formik.setFieldValue(name, e.target.value)
         }}
+        onBlur={() => {
+          void formik.setFieldTouched(name)
+        }}
         value={value}
         name={label}
         id={label}
       />
+      {!!error && !!touch && <div style={{ color: 'red' }}>{error}</div>}
     </div>
   )
 }
--- webapp/src/pages/NewPostPage/index.tsx
+++ webapp/src/pages/NewPostPage/index.tsx
@@ -1,4 +1,6 @@
 import { useFormik } from 'formik'
+import { withZodSchema } from 'formik-validator-zod'
+import { z } from 'zod'
 import { Input } from '../../components/Input/index'
 import { Segment } from '../../components/Segment'
 import { Textarea } from '../../components/Textarea/index'
@@ -10,6 +12,33 @@ export const AddNewPost = () => {
       foto: '',
       text: '',
     },
+    validate: withZodSchema(
+      z.object({
+        nickName: z
+          .string()
+          .min(1, 'Nickname is required')
+          .regex(/^[a-z0-9-]+$/, 'Nickname can contain only lowercase letters, numbers and dashes'),
+        foto: z.string().min(1, 'Nickname is required'),
+        text: z.string().min(100, 'Text should be at least 100 characters long'),
+      })
+    ),
+    // validate: (values) => {
+    //   const errors: Partial<typeof values> = {}
+    //   if (!values.nickName) {
+    //     errors.nickName = 'Nickname is required'
+    //   } else if (!values.nickName.match(/^[a-z0-9-]+$/)) {
+    //     errors.nickName = 'Nickname can contain only lowercase letters, numbers and dashes'
+    //   }
+    //   if (!values.foto) {
+    //     errors.foto = 'Foto is required'
+    //   }
+    //   if (!values.text) {
+    //     errors.text = 'Text is required'
+    //   } else if (values.text.length < 100) {
+    //     errors.text = 'Text should be at least 100 characters long'
+    //   }
+    //   return errors
+    // },
     onSubmit: (values) => {
       console.info('Submitted', values)
     },
@@ -27,7 +56,7 @@ export const AddNewPost = () => {
         <Input name="foto" label="Foto" formik={formik} />
 
         <Textarea name="text" label="Text" formik={formik} />
-
+        {!!formik.submitCount && !formik.isValid && <div style={{ color: 'red' }}>Some fiels are invalid</div>}
         <button type="submit">Add post</button>
       </form>
     </Segment>
